5688dee40ea8742aeecc42f340e51c1e7b3adea4
Patrick Hignell <patrickhignell@hotmail.com>
2026-03-11 10:41:52 +1300

    add class methods

--- main.py
+++ main.py
@@ -596,24 +596,65 @@
 
 #static methods- a method that belongs to a class rather than any object from that class- uses decorator @staticmethod
 
-class Employee:
-  def __init__(self, name, position):
-    self.name = name
-    self.position = position
+# class Employee:
+#   def __init__(self, name, position):
+#     self.name = name
+#     self.position = position
 
-  def get_info(self):
-    return f"{self.name} = {self.position}"
+#   def get_info(self):
+#     return f"{self.name} = {self.position}"
   
-  @staticmethod
-  def is_valid_position(position):
-    valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
-    return position in valid_positions
+#   @staticmethod
+#   def is_valid_position(position):
+#     valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
+#     return position in valid_positions
 
-employee1 = Employee("Spongebob", "Cook")
-employee2 = Employee("Sandy", "Rocket Scientist")
+# employee1 = Employee("Spongebob", "Cook")
+# employee2 = Employee("Sandy", "Rocket Scientist")
 
-print(employee1.get_info())
-print(Employee.is_valid_position(employee1.position))
+# print(employee1.get_info())
+# print(Employee.is_valid_position(employee1.position))
 
-print(employee2.get_info())
-print(Employee.is_valid_position(employee2.position))
+# print(employee2.get_info())
+# print(Employee.is_valid_position(employee2.position))
+
+
+# Class methods - Allow operations related to the class itself
+#               - Take (cls) as the first parameter, which represents the class itself
+
+# instance methods- best for operations on instances of the class (objects
+# static methods- best for utility functions that do not need access to class data
+# class methods - best for class-level data or require access to the class itself
+
+class Student:
+  count = 0
+  total_gpa = 0
+
+  def __init__(self, name, gpa):
+    self.name = name
+    self.gpa = gpa
+    Student.count += 1
+    Student.total_gpa += gpa
+
+  #INSTANCE METHOD
+  def get_info(self):
+    return f"{self.name} {self.gpa}"
+  
+  @classmethod
+  def get_count(cls):
+    return f"Total # of students: {cls.count}"
+  
+  @classmethod
+  def get_average_gpa(cls):
+    if cls.count == 0:
+      return 0
+    else:
+      return f"Average gpa: {cls.total_gpa / cls.count:.2f}"
+
+  
+student1 = Student("Spongebob", 3.2)
+student2 = Student("Patrick", 2.0)
+student3 = Student("Sandy",4.0)
+
+print(Student.get_count())
+print(Student.get_average_gpa())
